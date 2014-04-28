edd45d4b8e8f63952259c380a9ae95e438111fcf
Clicia <yuyuvn@mac.com>
2014-04-28 22:42:07 +0700

    update

--- src/englishlearning/presenter/Login.java
+++ src/englishlearning/presenter/Login.java
@@ -8,8 +8,11 @@ package englishlearning.presenter;
 
 import englishlearning.model.UsersList;
 import englishlearning.util.DataInDisk;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -21,7 +24,7 @@ import org.controlsfx.control.textfield.TextFields;
  * @author Clicia
  */
 public class Login extends Presenter {
-    @FXML TextField username;
+    @FXML TextField usernameTextField;
     
     //<editor-fold defaultstate="collapsed" desc="Property isLoged">
     private final BooleanProperty isLoged = new SimpleBooleanProperty(this, "isLoged", false);;
@@ -29,14 +32,22 @@ public class Login extends Presenter {
     public final void setIsLoged(Boolean value) { isLoged.set(value); }
     public final BooleanProperty isLogedProperty() { return isLoged; }
 //</editor-fold>
+    //<editor-fold defaultstate="collapsed" desc="Property username">
+    private final StringProperty username = new SimpleStringProperty(this, "username");;
+    public final String getUsername() { return username.get(); }
+    public final void setUsername(String value) { username.set(value); }
+    public final StringProperty usernameProperty() { return username; }
+//</editor-fold>
     
     private final UsersList users;
     
     public Login() {
         loadFXML();
         
+        Bindings.bindBidirectional(username, usernameTextField.textProperty());
+        
         users = DataInDisk.getUsersList();
-        TextFields.bindAutoCompletion(username, users);
+        TextFields.bindAutoCompletion(usernameTextField, users);
         
         isLogedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
             users.add(getUsername());
@@ -46,9 +57,5 @@ public class Login extends Presenter {
     
     public final void onLogin(ActionEvent event) {
         if (!getUsername().equals("")) setIsLoged(true);
-    }
-    
-    public final String getUsername() {
-        return username.getText();
     }
 }
