30416994a1ff006babf6683565ccbcf16d47fba6
Clicia <yuyuvn@mac.com>
2014-05-01 17:06:45 +0700

    Update

---
+++ src/englishlearning/model/wrapper/WrapperProperty.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package englishlearning.model.wrapper;
+
+import javafx.beans.property.SimpleObjectProperty;
+
+/**
+ *
+ * @author Clicia
+ * @param <T>
+ */
+public class WrapperProperty<T extends Wrapper> extends SimpleObjectProperty<T> {
+
+    public WrapperProperty() {
+    }
+
+    public WrapperProperty(T initialValue) {
+        super(initialValue);
+    }
+
+    public WrapperProperty(Object bean, String name) {
+        super(bean, name);
+    }
+
+    public WrapperProperty(Object bean, String name, T initialValue) {
+        super(bean, name, initialValue);
+    }
+    
+    @Override
+    public void fireValueChangedEvent() {
+        getValue().setChanged(true);
+        super.fireValueChangedEvent();
+        getValue().setChanged(false);
+    }
+}
