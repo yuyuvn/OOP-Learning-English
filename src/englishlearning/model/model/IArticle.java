2cc3d0cf23a404f24506ecf89623a3aa47be6a13
LVBK <levanbaokhanh.bkhn@gmail.com>
2014-05-08 17:53:03 +0700

    Update IArticle.java

--- src/englishlearning/model/model/IArticle.java
+++ src/englishlearning/model/model/IArticle.java
@@ -1,6 +1,7 @@
 package englishlearning.model.model;
 
 import englishlearning.model.Article;
+import java.util.List;
 import javafx.scene.image.Image;
 
 
@@ -13,4 +14,18 @@ public interface IArticle extends IWrapper {
     
     Image getThumbnail();
     String getParsedContent();
+    public String getGuid();
+    public void setGuid(String guid);
+    public String getTitle();
+    public void setTitle(String title);
+    public String getDescription();
+    public void setDescription(String description);
+    public String getLink();
+    public void setLink(String link);
+    public List getTags();
+    public void setTags(List tags);
+    public String getImageUrl();
+    public void setImageUrl(String imageUrl);
+    public String getContent();
+    public void setContent(String content);
 }
