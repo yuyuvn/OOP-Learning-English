a9a722f04666cf8e8706a75f22d1b588483a5e3d
LVBK <levanbaokhanh.bkhn@gmail.com>
2014-05-08 17:53:50 +0700

    Update IUser.java

--- src/englishlearning/model/model/IUser.java
+++ src/englishlearning/model/model/IUser.java
@@ -6,6 +6,8 @@
 
 package englishlearning.model.model;
 
+import englishlearning.model.PlayState;
+import englishlearning.model.ReadList;
 import englishlearning.model.UserInfo;
 
 /**
@@ -14,4 +16,12 @@ import englishlearning.model.UserInfo;
  */
 public interface IUser extends IWrapper {
     public UserInfo getUser();
+    public String getName();
+    public void setName(String name);
+    public String getLastUsed();
+    public void setLastUsed(String lastUsed);
+    public ReadList getReadList();
+    public void setReadList(ReadList readList);
+    public PlayState getPlayState();
+    public void setPlayState(PlayState playState);
 }
